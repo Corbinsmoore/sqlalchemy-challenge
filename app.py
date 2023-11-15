8abffd67bc67e801d8bcd9b63c855eaeace8304a
corbin moore <127463183+Corbinsmoore@users.noreply.github.com>
2023-11-14 18:42:03 -0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,52 @@
+# Import the dependencies.
+from flask import Flask
+import numpy as np 
+import datetime as dt 
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+#################################################
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+#################################################
+
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
+    )
+
